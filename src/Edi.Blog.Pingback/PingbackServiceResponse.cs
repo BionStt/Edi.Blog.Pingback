233109b6808e6c0c865636aa6d1d55ad85c89834
Edi Wang <Edi.Wang@outlook.com>
2018-12-27 20:12:33 +0800

    Open source this module

---
+++ src/Edi.Blog.Pingback/PingbackServiceResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace Edi.Blog.Pingback
+{
+    public enum PingbackServiceResponse
+    {
+        Success,
+        GenericError,
+        TerminatedMethodNotFound,
+        TerminatedUrlNotFound,
+        Error32TargetUriNotExist,
+        Error48PingbackAlreadyRegistered,
+        Error17SourceNotContainTargetUri,
+        SpamDetectedFakeNotFound,
+        ValidPingRequest,
+        InvalidPingRequest
+    }
+}
