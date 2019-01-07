b97b871484b419bb9644a4b83c3a1483babce3a8
Edi Wang <Edi.Wang@outlook.com>
2019-01-07 16:18:53 +0800

    add mvc extensions

---
+++ src/Edi.Blog.Pingback.MvcExtensions/AddPingbackHeaderAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace Edi.Blog.Pingback.MvcExtensions
+{
+    public class AddPingbackHeaderAttribute : ResultFilterAttribute
+    {
+        private readonly string _pingbackEndpoint;
+
+        public AddPingbackHeaderAttribute(string pingbackEndpoint)
+        {
+            _pingbackEndpoint = pingbackEndpoint;
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            context.HttpContext.Response.Headers.Add("x-pingback",
+                new[]
+                {
+                    $"{context.HttpContext.Request.Scheme}://{context.HttpContext.Request.Host}/{_pingbackEndpoint}"
+                });
+
+            base.OnResultExecuting(context);
+        }
+    }
+}
